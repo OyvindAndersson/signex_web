9ae09422fceed95df038f1dd3ddc1a800c56d0c6
Øyvind Andersson <mroyvindandersson@gmail.com>
2017-09-01 21:09:30 +0200

    Project view small change.

--- resources/assets/js/components/project/projects-view.jsx
+++ resources/assets/js/components/project/projects-view.jsx
@@ -34,15 +34,119 @@ export default class ProjectsView extends Component {
     }
 }
 
+export class OrderList extends Component {
+    constructor(props){
+        this.state = {
+            orders: props.orders
+        }
+    }
+
+    componentWillReceiveProps(props){
+        this.setState({orders: props.orders});
+    }
+    render(){
+
+        const orders = this.state.orders.map(order => {
+            return(
+                <option key={order.id} value={order.id}>{order.id} - {/* Also show order client */}</option>
+            );
+        });
+
+        return (
+            <select>
+                {this.props.children}
+            </select>
+        );
+    }
+}
+
 export class ProjectCreateForm extends Component {
     constructor(props){
         super(props);
+
+        this.handleOrderChanged = this.handleOrderChanged.bind(this);
+        this.state = {
+
+        }
+    }
+
+    handleInputChange(e){
+        const target = e.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        console.log(target.type);
+        console.log(value);
+
+        this.setState({ [name]: value });
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+
+        const url = signex.projectsPostUrl;
+        let data = {
+        };
+
+        axios.post(url, data)
+        .then(response => {
+            if(response.status !== 200){
+                console.log("Failed to submit new project.:");
+                console.log(response);
+            } else {
+                toast.success("Successfully added project id:"+response.data.project.id);
+            }
+            // on order added handler - if any need
+            // clear form fields
+            console.log(response);
+        })
+        .catch(error => {
+            /** @todo Display laravel errors with dedicated component */
+            let errors = error.response.data.errors;
+            if(errors){
+                for(let key in errors){
+                    toast.error(errors[key][0], {autoClose: 5000});
+                }
+            }
+            /** @todo Remove (debug) */
+            console.log("axios: " + error);
+            console.log(error);
+        });
+    }
+
+    handleOrderChanged(e){
+
     }
 
     render(){
         return (
-            <div>
-                
+            <div className="row">
+                <div className="col-md-6">
+                    <h4>Create order</h4>
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="row">
+                            <div className="col-md-12">
+
+                                <div className="form-group">
+                                    <label>Attach to order</label>
+                                    <OrderList onChange={this.handleOrderChanged} />
+                                </div>
+
+                                <div className="form-group">
+                                </div>
+
+                                <input type="submit" className="btn btn-primary" value="Submit" />
+                            </div>
+                            
+                        </div>
+                    </form>
+                </div>
+
+                <div className="col-md-6">
+                    <div className="jumbotron">
+                        <h3>New client form</h3>
+                        <ClientForm onClientAddedHandler={this.onClientAddedHandler} />
+                    </div>
+                </div>
             </div>
         );
     }
